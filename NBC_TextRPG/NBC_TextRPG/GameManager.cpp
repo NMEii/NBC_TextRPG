0df6732a4c1b6f645753d29a1111a342582066fd
minsung <99423006+zarame3636@users.noreply.github.com>
2025-12-29 20:47:00 +0900

    make frame of GM functions

--- NBC_TextRPG/NBC_TextRPG/GameManager.cpp
+++ NBC_TextRPG/NBC_TextRPG/GameManager.cpp
@@ -1,1 +1,60 @@
+#include "pch.h"
 #include "GameManager.h"
+
+GameManager::GameManager()
+{
+}
+
+GameManager::~GameManager()
+{
+}
+
+void GameManager::StartGame()
+{
+	bIsRunning = true;
+
+	Initizlize(); 
+
+	while (bIsRunning)
+	{
+		Render(); 
+
+		Update(); 
+	}
+
+}
+
+void GameManager::Initizlize()
+{
+}
+
+void GameManager::Update()
+{
+}
+
+void GameManager::Render()
+{
+}
+
+void GameManager::ShutDown()
+{
+	bIsRunning = false;
+}
+
+void GameManager::Delay(float Time)
+{
+	Sleep(Time * 1000); 
+}
+
+void GameManager::ClearConsole()
+{
+	system("cls");
+}
+
+void GameManager::BindUIEvents()
+{
+}
+
+void GameManager::HandleUIRequest(UIRequest req)
+{
+}
---
+++ NBC_TextRPG/NBC_TextRPG/UIRequest.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+
+enum class UIRequest
+{
+	None,
+	OpenInventory,
+	ExitGame,
+
+};
+
