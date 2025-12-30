e12352c73d2e228668f6e1a6c3c5a2964a496ac2
ha2m2 <hamin0691@gmail.com>
2025-12-30 17:57:20 +0900

    test Character.h/Character.cpp

--- NBC_TextRPG/NBC_TextRPG/Character.cpp
+++ NBC_TextRPG/NBC_TextRPG/Character.cpp
@@ -1,2 +1,4 @@
 #include "pch.h"
 #include "Character.h"
+
+using namespace std;
--- NBC_TextRPG/NBC_TextRPG/Character.h
+++ NBC_TextRPG/NBC_TextRPG/Character.h
@@ -1,7 +1,28 @@
 #pragma once
-#include "ICombat.h"
 
-class Character : public ICombat
+class Character
 {
+public:
+	string sName;
+	int iHp;
+	int iMaxHp;
+	int iAtk;
+
+	Character(string name, int maxHp, int hp, int atk) :
+		sName(name), iMaxHp(maxHp), iHp(hp), iAtk(atk) {
+	}
+
+	string GetName() const {
+		return sName;
+	}
+	int GetHp() const {
+		return iHp;
+	}
+	void SetHp(int hp) {
+		iHp = hp;
+	}
+
+	virtual void Attack(Character* monster) = 0;
+	virtual void UseItem() = 0;
 };
 
