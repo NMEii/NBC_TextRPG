225a49b15c18eb4b23c920dd2e82c650b3e8c044
ByungKwon Hwang <noaenix@gmail.com>
2025-12-31 14:13:11 +0900

    GM
    Generate_monster

---
+++ NBC_TextRPG/NBC_TextRPG/Generate_monster.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include"pch.h"
+#include"Monster.h"
+
+
+class Generate_monster
+{
+public:
+	static Monster* generate(int playerLevel);
+
+private:
+	static std::vector<std::string> monsterName;
+};
+
