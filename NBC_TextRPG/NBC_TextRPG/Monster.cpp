f620ce1800f994c9b40bd1f50724b851fedfda85
ByungKwon Hwang <noaenix@gmail.com>
2025-12-31 14:15:48 +0900

    Monster

---
+++ NBC_TextRPG/NBC_TextRPG/Monster.cpp
@@ -0,0 +1,13 @@
+﻿#include "Monster.h"
+#include<iostream>
+
+//입력 받을때
+Monster::Monster(std::string name, int playerLevel) :Character(name)
+{
+	level = playerLevel;
+	health = playerLevel * 50;
+	maxHealth = playerLevel * 50;
+	attack = playerLevel * 15;
+}
+
+
---
+++ NBC_TextRPG/NBC_TextRPG/Monster.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include"Character.h"
+
+class Monster :public Character
+{
+public:
+	Monster(std::string name, int level);
+};
