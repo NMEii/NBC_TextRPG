602c2a3e28148628d970ded34d14a8e499b97c0f
JRoLee <cnsfw123@gmail.com>
2025-12-31 19:38:50 +0900

    UseItemAction 수정
    UseItemAction 수정
    if문 else if문 switch문으로 변경

--- NBC_TextRPG/NBC_TextRPG/UseItemAction.cpp
+++ NBC_TextRPG/NBC_TextRPG/UseItemAction.cpp
@@ -12,28 +12,25 @@ void UseItemAction::Play(const ActionContext& context)
 	//사용할 아이템 갯수 확인
 	if (context.useItem->GetiItemCount() <= 0)
 		return;
-	
-	//힐링포션 사용
-	if (context.useItem->GetEffectType() == ItemEffectType::Heal)
+
+	switch (context.useItem->GetEffectType())
 	{
+	case ItemEffectType::Heal:
+
 		//currentHP 증가
 		context.owner->stats.currentHealth += context.useItem->GetEffectValue();
 
 		//HP 초과 회복시 max값으로 재조정
 		if (context.owner->stats.currentHealth > context.owner->stats.maxHealth)
-			context.owner->stats.currentHealth = context.owner->stats.currentHealth;
+		{
+			context.owner->stats.currentHealth = context.owner->stats.maxHealth;
+		}
 
-		//아이템 사용시 갯수 -1
-		context.useItem->SetsItemCount(context.useItem->GetiItemCount()-1);
-	}
-
-	//공격력 상승 포션 사용
-	else if (context.useItem->GetEffectType() == ItemEffectType::AttackUp)
-	{
+	case ItemEffectType::AttackUp:
 		//attack 증가
 		context.owner->stats.attack += context.useItem->GetEffectValue();
-
-		//아이템 사용시 갯수 -1
-		context.useItem->SetsItemCount(context.useItem->GetiItemCount() - 1);
 	}
+
+	//아이템 사용시 갯수 -1
+	context.useItem->SetsItemCount(context.useItem->GetiItemCount() - 1);
 }
