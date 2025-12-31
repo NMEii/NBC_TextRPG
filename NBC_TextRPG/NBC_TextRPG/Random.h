eb8ccd1e8d3ec74e05f160703a3515dcb54dd204
ha2m2 <hamin0691@gmail.com>
2025-12-31 12:49:50 +0900

    add Random.h

---
+++ NBC_TextRPG/NBC_TextRPG/Random.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <random>
+using namespace std;
+
+// std처럼 Random::Choice(), Random::Success() 형태로 사용
+namespace Random {
+	static random_device rd;
+	static mt19937 mt(rd());
+
+	//min 이상 max 이하의 정수 난수 반환
+	inline int Choice(int min, int max) {
+		uniform_int_distribution<int> dist(min, max);
+		return dist(mt);
+	}
+
+	//percentage 확률로 bool 반환 (ex. 0.3 -> 30% 확률로 true 반환)
+	inline bool Success(int percentage) {
+		bernoulli_distribution dist(percentage);
+		return dist(mt);
+	}
+}
