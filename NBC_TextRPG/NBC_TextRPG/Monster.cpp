f15394ba02358a1943e2c56554fbea5322f704bb
ByungKwon Hwang <noaenix@gmail.com>
2026-01-04 19:44:44 +0900

    spawn_monster

--- NBC_TextRPG/NBC_TextRPG/Monster.cpp
+++ NBC_TextRPG/NBC_TextRPG/Monster.cpp
@@ -11,8 +11,8 @@ Monster::Monster()
 Monster::Monster(std::string name, int playerLevel, int _artIndex) :Character(name)
 {
     artIndex = _artIndex;
-	stats.currentHealth = playerLevel * Random::Choice(20, 30);
-	stats.maxHealth = playerLevel * Random::Choice(20, 30);
+    stats.maxHealth = playerLevel * Random::Choice(20, 30);
+    stats.currentHealth = stats.maxHealth;
 	stats.attack = playerLevel * Random::Choice(5, 10);
 
 	AddActions(std::make_unique<AttackAction>());
